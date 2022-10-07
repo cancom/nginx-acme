d192b55ae7b89f3eba1420086166a338707ef73e
Sina Anvari <sina.anvari@cancom.de>
2022-10-07 12:13:05 +0200

    style: shellcheck

--- files/manage-vhosts.sh
+++ files/manage-vhosts.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# vim: set ft=sh :
+# shellcheck disable=SC1117,SC2181
 
 display_help()
 {
@@ -35,9 +37,9 @@ fi
 
 if [ "$1" != "--find-my-ip" ]; then
   DOMAIN=$2
-  VALIDATE_HOSTNAME=$( echo $DOMAIN | grep -P '(?=^.{1,254}$)(^(?>(?!\d+\.)[a-zA-Z0-9_\-]{1,63}\.?)+(?:[a-zA-Z]{2,})$)' )
+  VALIDATE_HOSTNAME=$( echo "${DOMAIN}" | grep -P '(?=^.{1,254}$)(^(?>(?!\d+\.)[a-zA-Z0-9_\-]{1,63}\.?)+(?:[a-zA-Z]{2,})$)' )
   if [[ -z $VALIDATE_HOSTNAME ]]; then
-    say @b@red[["Invalid hostname: $DOMAIN"]]
+    say @b@red[["Invalid hostname: ${DOMAIN}"]]
     say @b@red[["Exiting.."]]
     exit 1
   fi
@@ -45,25 +47,25 @@ fi
 
 create_vhost ()
 {
-  if [ -f "/etc/nginx/sites-available/$DOMAIN.conf" ]; then
-    say @b@red[["vHost for $DOMAIN already exists."]]
-    say @b@red[["You may remove it using manage-vhosts --delete $DOMAIN before trying again."]]
+  if [ -f "/etc/nginx/sites-available/${DOMAIN}.conf" ]; then
+    say @b@red[["vHost for ${DOMAIN} already exists."]]
+    say @b@red[["You may remove it using manage-vhosts --delete ${DOMAIN} before trying again."]]
     say @b@red[["Exiting.."]]
     exit 1
   fi
-  acme.sh --issue --domain $DOMAIN --webroot /var/www/_letsencrypt --keylength 4096
+  acme.sh --issue --domain "${DOMAIN}" --webroot /var/www/_letsencrypt --keylength 4096
   if [ $? -ne 0 ]; then
     echo
-    say @b@red[["Unable to obtain certificate for $DOMAIN!"]]
+    say @b@red[["Unable to obtain certificate for ${DOMAIN}!"]]
     say @b@red[["Exiting.."]]
     exit 1
   fi
   echo
-  say @b@green[["Successfully obtain certificate for $DOMAIN."]]
-  mkdir -p /var/www/$DOMAIN/{logs,html}
+  say @b@green[["Successfully obtain certificate for ${DOMAIN}."]]
+  mkdir -p /var/www/"${DOMAIN}"/{logs,html}
   echo "Creating vHost"
-  sed "s|subdomain.example.com|$DOMAIN|g" /etc/nginx/vhost.template > /etc/nginx/sites-available/$DOMAIN.conf
-  ln -s /etc/nginx/sites-available/$DOMAIN.conf /etc/nginx/sites-enabled/$DOMAIN.conf
+  sed "s|subdomain.example.com|${DOMAIN}|g" /etc/nginx/vhost.template > /etc/nginx/sites-available/"${DOMAIN}".conf
+  ln -s /etc/nginx/sites-available/"${DOMAIN}".conf /etc/nginx/sites-enabled/"${DOMAIN}".conf
   echo "Checking if config is OK and reloading NGINX"
   nginx -t && /etc/init.d/nginx reload
   if [ $? -ne 0 ]; then
@@ -78,35 +80,35 @@ create_vhost ()
 
 delete_vhost ()
 {
-  if [ ! -f /etc/nginx/sites-available/$DOMAIN.conf ] || [ ! -d /acmecerts/$DOMAIN ]; then
-    say @yellow[["No vHost or certificate found for $DOMAIN."]]
+  if [ ! -f /etc/nginx/sites-available/"${DOMAIN}".conf ] || [ ! -d /acmecerts/"$DOMAIN" ]; then
+    say @yellow[["No vHost or certificate found for ${DOMAIN}."]]
     say @yellow[["Nothing to do. Exiting.."]]
     exit
   fi
-  say @b@red[["This will remove vHost configuration and certificates for $DOMAIN."]]
+  say @b@red[["This will remove vHost configuration and certificates for ${DOMAIN}."]]
   say @b@red[["This is irreversible!"]]
   read -p "Are you REALLY REALLY sure? " -n 1 -r
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]; then
-      rm /etc/nginx/sites-available/$DOMAIN.conf /etc/nginx/sites-enabled/$DOMAIN.conf
-      acme.sh --revoke $DOMAIN
-      acme.sh --remove $DOMAIN
-      rm -rf /acmecerts/$DOMAIN
+      rm /etc/nginx/sites-available/"${DOMAIN}".conf /etc/nginx/sites-enabled/"${DOMAIN}".conf
+      acme.sh --revoke "${DOMAIN}"
+      acme.sh --remove "${DOMAIN}"
+      rm -rf /acmecerts/"${DOMAIN}"
   fi
 }
 
 test_certificate_staging ()
 {
-  acme.sh --issue --domain $DOMAIN --webroot /var/www/_letsencrypt --keylength 4096 --test
+  acme.sh --issue --domain "${DOMAIN}" --webroot /var/www/_letsencrypt --keylength 4096 --test
   if [ $? -ne 0 ]; then
     echo
-    say @b@red[["Unable to obtain certificate for $DOMAIN!"]]
+    say @b@red[["Unable to obtain certificate for ${DOMAIN}!"]]
     say @b@red[["Exiting.."]]
     exit 1
   fi
   echo
-  say @green[["Successfully obtained a test (Let's Encrypt Staging) certificate for $DOMAIN!"]]
-  say @b@green[["You may now go ahead and use: manage-vhosts --create $DOMAIN!"]]
+  say @green[["Successfully obtained a test (Let's Encrypt Staging) certificate for ${DOMAIN}!"]]
+  say @b@green[["You may now go ahead and use: manage-vhosts --create ${DOMAIN}!"]]
 }
 
 find_public_ip ()
